2a44bc5b8a0312fe90eacfdd34892c7e4c16606b
DESKTOP-MC41BAJ\Tom <tom.engmann@students.hs-mainz.de>
2024-10-25 03:50:37 +0200

    Grid Tracking

--- Assets/Scripts/GridPlacementSystem.cs
+++ Assets/Scripts/GridPlacementSystem.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class GridPlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    private GameObject mouseIndicator;
+    private GameObject mouseIndicator, cellIndicator;
 
     [SerializeField]
     private GridMouseInput gridMouseInput;
+
+    [SerializeField]
+    private Grid grid;
     private void Update()
     {
         Vector3 mousePos = gridMouseInput.GetSelectedMapPos();
+        Vector3Int gridPosition = grid.WorldToCell(mousePos);
         mouseIndicator.transform.position = mousePos;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 }
