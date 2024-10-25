197fd56f643358929ac7ac5d3c8d316fdb9c13cb
DESKTOP-MC41BAJ\Tom <tom.engmann@students.hs-mainz.de>
2024-10-25 12:20:50 +0200

    kleine anpassung für grid

--- Assets/Scripts/GridMouseInput.cs
+++ Assets/Scripts/GridMouseInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class GridMouseInput : MonoBehaviour
@@ -7,21 +8,22 @@ public class GridMouseInput : MonoBehaviour
     [SerializeField]
     private Camera sceneCamera;
 
-    private Vector3 lastPos;
+    private UnityEngine.Vector3 lastPos;
 
     [SerializeField]
     private LayerMask placementLayermask;
 
-    public Vector3 GetSelectedMapPos()
+    public UnityEngine.Vector3 GetSelectedMapPos()
     {
-        Vector3 mousePos = Input.mousePosition;
+        UnityEngine.Vector3 mousePos = Input.mousePosition;
         mousePos.z = sceneCamera.nearClipPlane;
         Ray ray = sceneCamera.ScreenPointToRay(mousePos);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 1000, placementLayermask))
+        if (Physics.Raycast(ray, out hit, 1000, placementLayermask) && hit.collider.CompareTag("GridTag"))
         {
             lastPos = hit.point;
         }
         return lastPos;
+        
     }
 }
