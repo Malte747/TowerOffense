2f875b428e85e6d39ef834f0495fe39ab4ec23f5
Malteleh <148697349+Malteleh@users.noreply.github.com>
2024-10-27 16:23:55 +0100

    Camera complete

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,37 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     public Transform cameraTransform;
     public float normalSpeed;
     public float fastSpeed;
     public float movementSpeed;
     public float movementTime;
     public float rotationAmount;
-    public Vector3 zoomAmount;
+    public float maxZoom;
+    public float minZoom;
+    [SerializeField] public Vector3 zoomAmount;
 
-    public Vector3 newPosition;
-    public Quaternion newRotation;
-    public Vector3 newZoom;
+    [SerializeField] public Vector3 newPosition;
+    [SerializeField] public Quaternion newRotation;
+    [SerializeField] public Vector3 newZoom;
 
-    public Vector3 dragStartPosition;
-    public Vector3 dragCurrentPosition;
+    [SerializeField] public Vector3 dragStartPosition;
+    [SerializeField] public Vector3 dragCurrentPosition;
 
+    [SerializeField] private Quaternion startRotation;
+
+    public float edgeScrollSpeed = 10f;
+    public float edgeThickness = 10f;
+
+    public Vector3 minPosition;
+    public Vector3 maxPosition;
 
     void Start()
     {
-        
         newPosition = transform.position;
+        startRotation = transform.rotation;
         newRotation = transform.rotation;
         newZoom = cameraTransform.localPosition;
 
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
-    
     void Update()
     {
         HandleMouseInput();
@@ -40,37 +49,32 @@ public class CameraController : MonoBehaviour
 
     void HandleMouseInput()
     {
-        if(Input.mouseScrollDelta.y !=0)
+        // Zoom mit dem Mausrad
+        if (Input.mouseScrollDelta.y != 0)
         {
             newZoom += Input.mouseScrollDelta.y * zoomAmount;
         }
 
-        if(Input.GetMouseButtonDown(2))
+        if (Input.GetMouseButtonDown(2))
         {
             Plane plane = new Plane(Vector3.up, Vector3.zero);
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            float entry;
-
-            if(plane.Raycast(ray, out entry))
+            if (plane.Raycast(ray, out float entry))
             {
                 dragStartPosition = ray.GetPoint(entry);
             }
         }
 
+        // Halten der mittleren Maustaste für Draggen
         if (Input.GetMouseButton(2))
         {
             Plane plane = new Plane(Vector3.up, Vector3.zero);
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            float entry;
-
-            if(plane.Raycast(ray, out entry))
+            if (plane.Raycast(ray, out float entry))
             {
                 dragCurrentPosition = ray.GetPoint(entry);
-
                 newPosition = transform.position + dragStartPosition - dragCurrentPosition;
             }
         }
@@ -78,52 +82,92 @@ public class CameraController : MonoBehaviour
 
     void HandleMovementInput()
     {
+        // Kamera-Geschwindigkeit erhöhen, wenn Shift gedrückt wird
         if (Input.GetKey(KeyCode.LeftShift))
         {
             movementSpeed = fastSpeed;
         }
         else
         {
-             movementSpeed = normalSpeed;
+            movementSpeed = normalSpeed;
         }
 
-        if(Input.GetKey(KeyCode.UpArrow))
+        // Kamera durch Tastatureingabe bewegen
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            newPosition += (transform.forward * movementSpeed);
+            newPosition += (transform.forward * movementSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            newPosition += (transform.forward * -movementSpeed);
+            newPosition += (transform.forward * -movementSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            newPosition += (transform.right * movementSpeed);
+            newPosition += (transform.right * movementSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            newPosition += (transform.right * -movementSpeed);
-        }
-
-        if(Input.GetKey(KeyCode.N))
-        {
-            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
-        }
-        if(Input.GetKey(KeyCode.M))
-        {
-            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+            newPosition += (transform.right * -movementSpeed * Time.deltaTime);
         }
 
-        if(Input.GetKey(KeyCode.R))
+        // Kamera durch Bildschirmrand-Scrolling bewegen
+        if (Input.mousePosition.x <= edgeThickness)
+        {
+            newPosition += transform.right * -edgeScrollSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= Screen.width - edgeThickness)
+        {
+            newPosition += transform.right * edgeScrollSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= edgeThickness)
+        {
+            newPosition += transform.forward * -edgeScrollSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y >= Screen.height - edgeThickness)
+        {
+            newPosition += transform.forward * edgeScrollSpeed * Time.deltaTime;
+        }
+
+        // Kamera-Rotation mit Tasten
+        if (Input.GetKey(KeyCode.N) || Input.GetKey(KeyCode.PageUp))
+        {
+        float rotationY = Mathf.Clamp(newRotation.eulerAngles.y + rotationAmount, startRotation.eulerAngles.y - 45f, startRotation.eulerAngles.y + 45f);
+        newRotation = Quaternion.Euler(newRotation.eulerAngles.x, rotationY, newRotation.eulerAngles.z);
+        }
+        else if (Input.GetKey(KeyCode.M) || Input.GetKey(KeyCode.PageDown))
+        {
+        float rotationY = Mathf.Clamp(newRotation.eulerAngles.y - rotationAmount, startRotation.eulerAngles.y - 45f, startRotation.eulerAngles.y + 45f);
+        newRotation = Quaternion.Euler(newRotation.eulerAngles.x, rotationY, newRotation.eulerAngles.z);
+        }
+        else
+        {
+        newRotation = Quaternion.Lerp(newRotation, startRotation, Time.deltaTime * movementTime);
+        }
+
+        //zoom mit tasten
+        if (Input.GetKey(KeyCode.Home))
         {
             newZoom += zoomAmount;
         }
-        if(Input.GetKey(KeyCode.F))
+        if (Input.GetKey(KeyCode.End))
         {
             newZoom -= zoomAmount;
         }
 
+
+        
+        newPosition.x = Mathf.Clamp(newPosition.x, minPosition.x, maxPosition.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minPosition.y, maxPosition.y);
+        newPosition.z = Mathf.Clamp(newPosition.z, minPosition.z, maxPosition.z);
+
+        
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
-        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+
+        
+        newZoom.y = Mathf.Clamp(newZoom.y, minZoom, maxZoom);
+        newZoom.z = Mathf.Clamp(newZoom.z, -maxZoom, -minZoom);
+        Vector3 targetPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = targetPosition;
     }
 }
