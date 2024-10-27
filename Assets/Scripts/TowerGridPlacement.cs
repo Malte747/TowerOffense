c6f42bc8b0507b913fa87e2d4e4c9c675253305b
DESKTOP-MC41BAJ\Tom <tom.engmann@students.hs-mainz.de>
2024-10-27 22:00:02 +0100

    Tower Placement + Felder Blockieren

--- Assets/Scripts/TowerGridPlacement.cs
+++ Assets/Scripts/TowerGridPlacement.cs
@@ -1,30 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerGridPlacement : MonoBehaviour
 {
     public List<Vector3Int> Occupied;
     public Material indicatorColor;
+    public GameObject indicator;
 
-    
-    public static bool t1x2, t2x1, t2x2;
-    public static int xSize, zSize;
+    [SerializeField]
+    private Grid grid;
+
+    public int xSize, zSize, placementRangeX, placementRangeZ;
+
+    public List<GameObject> Towers;
+
+
+    //Bis UI existiert Placeholder
+    public static int towerNumberUI;
 
     void Start()
     {
-        t1x2 = true;
-        t2x1 = true;
-        t2x2 = true;
-
-        xSize = 5;
-        zSize = 5;
+        indicator.transform.localScale = new Vector3 (xSize, 1, zSize);
     }
 
     void Update()
     {
-        bool redq = false;
+        bool hitTower = false;
         for (int i = 1; i <= xSize; i++)
         {
 
@@ -33,26 +40,75 @@ public class TowerGridPlacement : MonoBehaviour
                 Vector3Int gridCheck = new Vector3Int(GridPlacementSystem.gridPosition.x + i - 1, 0, GridPlacementSystem.gridPosition.z + i2 - 1);
                 if (Occupied.Contains(gridCheck))
                 {
-                    redq = true;
+                    hitTower = true;
                 }
             }
+        if(GridPlacementSystem.gridPosition.x + xSize -1 > placementRangeX || GridPlacementSystem.gridPosition.z + zSize - 1 > placementRangeZ)
+            {
+                hitTower = true;
+            }
         }
-        if (redq)
+        if (hitTower)
         {
-            Debug.Log("Position is occupied.");
-            indicatorColor.SetColor("_BaseColor", Color.red);
+            //Debug.Log("Position is occupied.");
+            indicatorColor.SetColor("_BaseColor", new Color(1f, 0f, 0f, 0.1f));
         }
         else
         {
             //Debug.Log("Position is free.");
-            indicatorColor.SetColor("_BaseColor", Color.green);
+            indicatorColor.SetColor("_BaseColor", new Color(0f, 1f, 0f, 0.1f));
         }
 
-        
+        if (Input.GetMouseButtonDown(0) && !hitTower)
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                PlaceTower(towerNumberUI);
+            }
+        }
+
+    }
+
+    public void PlaceTower(int number)
+    {
+        // OccupyCell(GridPlacementSystem.gridPosition);
+
+        for (int i = 1; i <= xSize; i++)
+        {
+
+            for (int i2 = 1; i2 <= zSize; i2++)
+            {
+                    OccupyCell(new Vector3Int(GridPlacementSystem.gridPosition.x + i -1, 0 , GridPlacementSystem.gridPosition.z + i2 - 1));
+                    //Debug.Log("Occupy");
+            }
+        }
+
+
+
+            Instantiate(Towers[number], grid.CellToWorld(GridPlacementSystem.gridPosition), Quaternion.identity);
     }
 
     public void OccupyCell(Vector3Int cellNumbers)
     {
         Occupied.Add(cellNumbers);
+    }
+
+    public void ChangeTowerWhilePlacing(string numbers)
+    {
+        string[] digits = Regex.Split(numbers, @"\D+");
+        List<int> ints = new List<int>();
+        foreach (string value in digits)
+        {
+            int number;
+            if (int.TryParse(value, out number))
+            {
+                ints.Add(number);
+            }
+        }
+
+        towerNumberUI = ints[0];
+        xSize = ints[1];
+        zSize = ints[2];
+        indicator.transform.localScale = new Vector3(xSize, 1, zSize);
     }
 }
