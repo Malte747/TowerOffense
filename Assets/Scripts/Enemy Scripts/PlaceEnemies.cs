d49e4a708e79cc0bce2b28786f884ac9b586b7c2
JohannaJosupeit <josupeitjohanna@gmail.com>
2024-12-09 12:29:37 +0100

    linked info texts when placing units incorrectly

--- Assets/Scripts/Enemy Scripts/PlaceEnemies.cs
+++ Assets/Scripts/Enemy Scripts/PlaceEnemies.cs
@@ -11,11 +11,14 @@ public class PlaceEnemies : MonoBehaviour
     public List<GameObject> indicatorUnits = new List<GameObject>();
     public Material indicatorColor;
     public GameObject indicatorEmpty;
+    private UIManager _uiManager;
 
     private void Start()
     {
         placingUnit = false;
         manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        _uiManager = GameObject.Find("UiManager").GetComponent<UIManager>();
     }
     void Update()
     {
@@ -32,31 +35,74 @@ public class PlaceEnemies : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             
-            if (Physics.Raycast(ray, out hit, 1000, planeLayer) && manager.attackersTurn)
+            if (Physics.Raycast(ray, out hit, 1000, planeLayer) 
+                    && unit.GetComponent<EnemyScript>().supplyCost + manager.attackerSupply <= manager.maxSupply
+                    && manager.attackersTurn && unit.GetComponent<EnemyScript>().cost <= manager.attackerGold)
             {
                 indicatorEmpty.transform.position = hit.point;
                 indicatorColor.SetColor("_BaseColor", new Color(0.09215922f, 0.838f, 0.04049486f, 0.5f));
-                if (Input.GetMouseButtonDown(0) && unit.GetComponent<EnemyScript>().cost <= manager.attackerGold
-                    && unit.GetComponent<EnemyScript>().supplyCost + manager.attackerSupply <= manager.maxSupply)
-                {
-                    Instantiate(unit, hit.point, Quaternion.identity);
-                    manager.UnitPayment(unit.GetComponent<EnemyScript>().cost);
-                    manager.UnitSupplyPayment(unit.GetComponent<EnemyScript>().supplyCost);
-                    if (!Input.GetKey(KeyCode.LeftShift))
+                
+                    if (Input.GetMouseButtonDown(0))
                     {
-                        placingUnit = false;
-                        foreach (GameObject unit in indicatorUnits)
+                        Instantiate(unit, hit.point, Quaternion.identity);
+                        manager.UnitPayment(unit.GetComponent<EnemyScript>().cost);
+                        manager.UnitSupplyPayment(unit.GetComponent<EnemyScript>().supplyCost);
+                        if (!Input.GetKey(KeyCode.LeftShift))
                         {
-                            unit.SetActive(false);
+                            placingUnit = false;
+                            foreach (GameObject unit in indicatorUnits)
+                            {
+                                unit.SetActive(false);
+                            }
                         }
-                    }
 
-                }
+                    }
+                
+                
+                    
+                
+                
+                
             }
             else if (Physics.Raycast(ray, out hit, 1000))
             {
                 indicatorColor.SetColor("_BaseColor", new Color(0.8392157f, 0.03921568f, 0.06320632f, 0.5f));
                 indicatorEmpty.transform.position = hit.point;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (manager.attackersTurn && unit.GetComponent<EnemyScript>().cost > manager.attackerGold)
+                    {
+                        _uiManager.NotEnoughGoldMessage();
+                    }
+                    if(unit.GetComponent<EnemyScript>().supplyCost + manager.attackerSupply > manager.maxSupply)
+                    {
+                        _uiManager.NotEnoughSupplyMessage();
+                    }
+                    if(!Physics.Raycast(ray, out hit, 1000, planeLayer))
+                    {
+                        _uiManager.CannotBuildHereMessage();
+                    }
+                    
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (manager.attackersTurn && unit.GetComponent<EnemyScript>().cost > manager.attackerGold)
+                    {
+                        _uiManager.NotEnoughGoldMessage();
+                    }
+                    if (unit.GetComponent<EnemyScript>().supplyCost + manager.attackerSupply > manager.maxSupply)
+                    {
+                        _uiManager.NotEnoughSupplyMessage();
+                    }
+                    if (!Physics.Raycast(ray, out hit, 1000, planeLayer))
+                    {
+                        _uiManager.CannotBuildHereMessage();
+                    }
+                }
             }
         }
         /*
